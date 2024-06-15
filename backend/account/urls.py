8f17dc71c249b913ec7e62aa654f7ffe1e80852b
odi1n <sashf1999@mail.ru>
2024-06-15 19:54:23 +0300

    feat(account): views user me

--- backend/account/urls.py
+++ backend/account/urls.py
@@ -5,7 +5,10 @@ from rest_framework_simplejwt.views import (
     TokenVerifyView,
 )
 
+from account.views import UserView
+
 urlpatterns = [
+    path("me/", UserView.as_view(), name="users"),
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
