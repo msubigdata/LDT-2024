516c83878dd1ae0b53b311efbf341a880c592628
amalkurbanbaev <amalkurbanbaev@yandex.ru>
2024-06-15 19:48:15 +0500

    feat: setup router context

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,23 +1,41 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion -- ignore assertion */
 import { StrictMode } from "react";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { createRouter, RouterProvider } from "@tanstack/react-router";
+import { createRouter, ErrorComponent, RouterProvider } from "@tanstack/react-router";
 import { createRoot } from "react-dom/client";
 import { ZodError } from "zod";
 
 import "@/styles/index.css";
+
 import { routeTree } from "./generated/routes";
+
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry(failureCount, error) {
+        if (error instanceof ZodError) {
+          return false;
+        }
+
+        return failureCount < 3;
+      },
+    },
+  },
+});
 
 const rootElement = document.getElementById("root") as HTMLDivElement;
 
 const router = createRouter({
   routeTree,
-  // with 'intent' preload, the router will preload the next route when the user hovers over a link,
-  // see: https://tanstack.com/router/latest/docs/framework/react/api/router/RouterOptionsType#defaultpreload-property
   defaultPreload: "intent",
-  // since we're using React Query, we don't want loader calls to ever be stale
-  // this will ensure that the loader is always called when the route is preloaded or visited
   defaultStaleTime: 0,
+  defaultErrorComponent: ({ error }) => <ErrorComponent error={error} />,
+  context: {
+    auth: undefined!, // We'll inject this when we render
+    queryClient,
+  },
 });
 
 declare module "@tanstack/react-router" {
@@ -28,21 +46,6 @@ declare module "@tanstack/react-router" {
 
 if (!rootElement.innerHTML) {
   const root = createRoot(rootElement);
-
-  const queryClient = new QueryClient({
-    defaultOptions: {
-      queries: {
-        refetchOnWindowFocus: false,
-        retry(failureCount, error) {
-          if (error instanceof ZodError) {
-            return false;
-          }
-
-          return failureCount < 3;
-        },
-      },
-    },
-  });
 
   root.render(
     <StrictMode>
--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,23 +1,21 @@
-import { createRootRouteWithContext, Link, Outlet } from "@tanstack/react-router";
+import { type QueryClient } from "@tanstack/react-query";
+import { createRootRouteWithContext, Outlet } from "@tanstack/react-router";
 
-import Logo from "@/assets/logo.svg?react";
-import { buttonVariants } from "@/components/ui/button";
-import { cn } from "@/lib/utils";
+import { NotFoundComponent } from "@/components/templates/not-fount";
+
+export interface Auth {
+  login: (username: string) => void;
+  logout: () => void;
+  status: "loggedOut" | "loggedIn";
+  username?: string;
+}
+
+export interface RouterContext {
+  auth: Auth;
+  queryClient: QueryClient;
+}
 
 export const Route = createRootRouteWithContext()({
   component: () => <Outlet />,
   notFoundComponent: () => <NotFoundComponent />,
 });
-
-function NotFoundComponent() {
-  return (
-    <div className="flex h-full flex-col items-center justify-center gap-10">
-      <Logo className="size-32" />
-      <h1 className="text-center text-3xl font-bold">Страница не найдена</h1>
-
-      <Link to="/" className={cn(buttonVariants({ variant: "default" }))}>
-        Перейти на главную страница
-      </Link>
-    </div>
-  );
-}
