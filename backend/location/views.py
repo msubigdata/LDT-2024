b08ba4c61a138342ddd9018abd002635e1ee73b4
odi1n <sashf1999@mail.ru>
2024-06-16 17:37:05 +0300

    feat(location): crud

--- backend/location/views.py
+++ backend/location/views.py
@@ -5,16 +5,19 @@ from location.models import Camera, File, Location
 from location.serializers import CameraSerializer, FileSerializer, LocationSerializer
 
 
-class LocationListView(generics.ListAPIView):
+class LocationListView(generics.ListAPIView, generics.CreateAPIView, generics.UpdateAPIView, generics.DestroyAPIView):
     serializer_class = LocationSerializer
     queryset = Location.objects.all()
+    http_method_names = ("get", "post", "patch", "delete")
 
 
-class CameraListView(generics.ListAPIView):
+class CameraListView(generics.ListAPIView, generics.CreateAPIView, generics.UpdateAPIView, generics.DestroyAPIView):
     serializer_class = CameraSerializer
     queryset = Camera.objects.all()
+    http_method_names = ("get", "post", "patch", "delete")
 
 
-class FileListView(generics.ListAPIView):
+class FileListView(generics.ListAPIView, generics.CreateAPIView, generics.UpdateAPIView, generics.DestroyAPIView):
     serializer_class = FileSerializer
     queryset = File.objects.all()
+    http_method_names = ("get", "post", "patch", "delete")
