31a64fa9f196140ccb9564af373ccaaf9f4b634c
odi1n <sashf1999@mail.ru>
2024-06-16 22:57:44 +0300

    refactor(api): style

--- backend/api/base.py
+++ backend/api/base.py
@@ -14,7 +14,15 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    re_path(r"^(?P<format>\.json|\.yaml)$", schema_view.without_ui(cache_timeout=0), name="schema"),
-    re_path(r"^$", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
-    re_path(r"^redoc/$", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+    re_path(
+        r"^(?P<format>\.json|\.yaml)$",
+        schema_view.without_ui(cache_timeout=0),
+        name="schema",
+    ),
+    re_path(
+        r"^$", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"
+    ),
+    re_path(
+        r"^redoc/$", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"
+    ),
 ]
