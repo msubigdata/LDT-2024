b000d0ee07a1a8c50d3c5a8f0d6808eb2dc8e6f5
amalkurbanbaev <amalkurbanbaev@yandex.ru>
2024-06-16 13:12:27 +0500

    fix: login callback passing & env

--- frontend/src/routes/_public/sign-in/route.tsx
+++ frontend/src/routes/_public/sign-in/route.tsx
@@ -38,7 +38,8 @@ function SignInComponent() {
   const search = Route.useSearch();
 
   const {
-    loginMutation: { mutateAsync: login, isPending: loginLoading, error },
+    loginMutation: { isPending: loginLoading, error },
+    login,
   } = useAuthContext();
 
   const form = useForm<UserAuth>({
@@ -50,10 +51,10 @@ function SignInComponent() {
   });
 
   const onSubmit = async (values: z.infer<typeof credentialsSchema>) => {
-    await login(values).then(async () => {
-      await router.invalidate();
-      return navigate({ to: search.redirect ?? fallback });
-    });
+    await login(values);
+
+    await router.invalidate();
+    await navigate({ to: search.redirect ?? fallback });
   };
 
   const isLoading = routeLoading || loginLoading;
